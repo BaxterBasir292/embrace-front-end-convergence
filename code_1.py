da01dd9b36bc6d3e7b32aad902a48541864f6573
Basir <140265607+BaxterBasir292@users.noreply.github.com>
2023-10-26 08:56:59 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,59 @@
+def generate_random_data():
+if __name__ == "__main__":
+    return data
+    for item in data:
+import random
+
+        print(f"Random Number: {item}")
+
+    main()
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+    data = generate_random_data()    for (let i = 0; i < 10; i++) {
+main();
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
+
+    });
+    }
+    data.forEach(item => {
+
+    const data = generateRandomData();
+        console.log("Random Number: " + item);
+}
+}
+    const data = [];
+    return data;
+function main() {    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+def main():
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+
+
+import random
+    return data
+
+    for item in data:
+def generate_random_data():function main() {
+    const data = [];
+}
+function generateRandomData() {
+    }
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+    return data;
+main();
+}
+    const data = generateRandomData();
+    });
+
+    data.forEach(item => {
+
+        console.log("Random Number: " + item);
